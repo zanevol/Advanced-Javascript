4cc5990cf105d6b7f878bb5a7d9f0ad2b21acc72
zanevol <zanevoll@gmail.com>
2021-05-09 20:12:25 +0700

    1. Добавить методы и обработчики событий для поля поиска. Создать в объекте данных поле searchLine и привязать к нему содержимое поля ввода. На кнопку «Искать» добавить обработчик клика, вызывающий метод FilterGoods. 2. Добавить корзину. В html-шаблон добавить разметку корзины. Добавить в объект данных поле isVisibleCart, управляющее видимостью корзины. 3. Добавлять в .goods-list заглушку с текстом «Нет данных» в случае, если список товаров пуст.

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+	el: '#app',
+	data: {
+		catalogUrl: '/catalogData.json',
+		products: [],
+		imgCatalog: 'https://via.placeholder.com/200x150',
+		searchLine: '',
+		isVisibleCart: false,
+	},
+	methods: {
+		getJson(url) {
+			return fetch(url)
+				.then(result => result.json())
+				.catch(error => {
+					console.log(error);
+				})
+		},
+		addProduct(product) {
+			console.log(product.id_product);
+		},
+		filterGoods(value) {
+			let filtered = [];
+			const regexp = new RegExp(value, 'i'); //
+			filtered = this.products.filter(product => regexp.test(product.product_name));
+			this.products.forEach(el => {
+				const block = document.querySelector(`.product-item[data-id="${el.id_product}"]`);
+				if (!filtered.includes(el)) {
+					block.classList.add('invisible');
+				} else {
+					block.classList.remove('invisible');
+				}
+			})
+		}
+	},
+	beforeCreate() { },
+	created() {
+		this.getJson(`${API + this.catalogUrl}`)
+			.then(data => {
+				for (let el of data) {
+					this.products.push(el);
+				}
+			});
+	},
+	beforeMount() { },
+	mounted() { },
+	beforeUpdate() { },
+	updated() { },
+	beforeDestroy() { },
+	destroyed() { },
+});
